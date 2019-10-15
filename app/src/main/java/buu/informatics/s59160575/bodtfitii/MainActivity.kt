d0977c7bdaffb0704ea516688d9b60488ce71e32
wittayakaew <59160039@go.buu.ac.th>
2019-10-15 20:20:58 +0700

    add home Fragment

--- app/src/main/java/buu/informatics/s59160575/bodtfitii/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160575/bodtfitii/MainActivity.kt
@@ -1,13 +1,15 @@
 package buu.informatics.s59160575.bodtfitii
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import buu.informatics.s59160575.bodtfitii.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
