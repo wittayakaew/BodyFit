cf86c6a3e0e2973efd45e850d3f6140680968df3
wittaya <59160039@go.buu.ac.th>
2019-10-30 18:21:32 +0700

    add radio button

--- app/src/main/java/buu/informatics/s59160575/bodtfitii/BmiFragment.kt
+++ app/src/main/java/buu/informatics/s59160575/bodtfitii/BmiFragment.kt
@@ -15,6 +15,7 @@ import buu.informatics.s59160575.bodtfitii.databinding.FragmentBmiBinding
  */
 class BmiFragment : Fragment() {
     private lateinit var binding: FragmentBmiBinding
+    var bmi = 0;
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,8 +26,22 @@ class BmiFragment : Fragment() {
             bmiBackButton.setOnClickListener{
                 findNavController().navigate(R.id.action_bmiFragment_to_homeFragment)
             }
+            bmiCalculatorButton.setOnClickListener{
+                calculatorBMI()
+            }
         }
         return binding.root
+    }
+
+    fun calculatorBMI(){
+        binding.apply {
+            var hight = hightEditBmiNum.text.toString().toInt()
+            var wight = wightEditBmiNum.text.toString().toInt()
+            var bmi= (wight/((hight*1.0/100)*(hight*1.0/100)))
+
+            bmiResultText.text = "%.2f".format(bmi).toDouble().toString()
+        }
+
     }
 
 
--- app/src/main/java/buu/informatics/s59160575/bodtfitii/CaloriesFragment.kt
+++ app/src/main/java/buu/informatics/s59160575/bodtfitii/CaloriesFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import buu.informatics.s59160575.bodtfitii.databinding.FragmentCaloriesBinding
@@ -20,14 +22,34 @@ class CaloriesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_calories,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calories, container, false)
         binding.apply {
-            caloriesBackButton.setOnClickListener{
+            caloriesBackButton.setOnClickListener {
                 findNavController().navigate(R.id.action_caloriesFragment_to_homeFragment)
-            }
+                }
+            caloriesCalButton.setOnClickListener {
+                    val id = genderGroupSelect.checkedRadioButtonId
+
+                if (genderFemale.isChecked){
+                    Toast.makeText( context, "female",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText( context, "male",Toast.LENGTH_SHORT).show()
+                }
+
+
+                }
+            return binding.root
         }
-        return binding.root
+
+        fun calculatorCalories() {
+            binding.apply {
+                var age = caloriesAgeNum.text.toString().toInt()
+                var hight = caloriesHightNum.text.toString().toInt()
+                var weight = caloriesWeightNum.text.toString().toInt()
+
+            }
+
+        }
+
     }
-
-
 }
